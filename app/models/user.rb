c50f0f9e5cc1de888b47208e46d861eac90a5307
Aayushi <kanungoaashi@gmail.com>
2022-05-18 19:12:55 +0530

    Add role of buyer and seller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum :role, { buyer: 0, seller: 1 }
 end
