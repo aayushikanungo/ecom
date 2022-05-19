9ad946bdd1cccdd658e7ca4a278121cd74a9fdde
Aayushi <kanungoaashi@gmail.com>
2022-05-19 15:38:08 +0530

    Add user_id to products

---
+++ db/migrate/20220519100147_add_user_to_products.rb
@@ -0,0 +1,5 @@
+class AddUserToProducts < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :products, :user, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_113012) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_100147) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -25,7 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_113012) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
