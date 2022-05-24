f930d043354e932dc2b37f88befc711053b6848b
Aayushi <kanungoaashi@gmail.com>
2022-05-24 13:22:31 +0530

    Added add, reduce and remove button from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,4 +11,26 @@ class CartsController < ApplicationController
     @cart_item.save
     redirect_to carts_path
   end
+
+  def add_quantity
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.quantity += 1
+    @cart_item.save
+    redirect_to carts_path
+  end
+  
+  def reduce_quantity
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.quantity > 1
+      @cart_item.quantity -= 1
+    end
+    @cart_item.save
+    redirect_to carts_path
+  end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to carts_path
+  end  
 end
